a0722a8e68c12babae03a592c4e8337a6eec4fe4
Goldfinch <facu_vila@hotmail.com>
2025-12-20 22:12:51 -0300

    nueva ruta creada

---
+++ app/user/recover/password/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+
+
+
+const page = () => {
+    return (
+        <div className='w-full h-screen sm:h-auto sm:max-w-md'>
+            hi recover password
+        </div>
+    );
+};
+
+export default page;
