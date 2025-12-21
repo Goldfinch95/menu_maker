1d366a2cc8aa74e63df1bb5d90a76f2fd6293631
Goldfinch <facu_vila@hotmail.com>
2025-12-20 22:06:06 -0300

    nueva ruta creada

---
+++ app/user/create/password/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+
+
+
+const page = () => {
+    return (
+        <div className='w-full h-screen sm:h-auto sm:max-w-md'>
+            hi create password
+        </div>
+    );
+};
+
+export default page;
