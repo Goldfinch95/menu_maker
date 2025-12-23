bd4c26c0ef05ef4faff0cf24d3c9febbb484fa28
Goldfinch <facu_vila@hotmail.com>
2025-12-23 13:57:02 -0300

    .

--- app/user/change/password/services/change_service.ts
+++ app/user/change/password/services/change_service.ts
@@ -5,30 +5,26 @@ import { passwordType } from "../types/password";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const userChangePasswordService = async (data: passwordType, token: string): Promise <{ message: string }> => {
-    try {
-        const response = await fetch(`${BASE_URL}/users/restore-password`, {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json",
-            },
-            body: JSON.stringify({
-                password: data.password,
-                confirmationPassword: data.password,
-                token: token,
-            }),
-        });
-        if(!response.ok){
-          if(response.status === 401){
-            throw new Error("Error 401");
-          }
-          else {
-            throw new Error("Error al iniciar sesión");
-          }  
-        }
-        return await response.json();
-      } catch (error) {
-        console.error("Error al autenticar usuario:", error);
-        throw error;
-      }
-}
+export const userChangePasswordService = async (
+  data: passwordType,
+  token: string
+): Promise<{ message: string }> => {
+  const response = await fetch(`${BASE_URL}/users/restore-password`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      token,
+      password: data.password,
+      confirmationPassword: data.password,
+    }),
+  });
+
+  if (!response.ok) {
+    if (response.status === 401) throw new Error("Error 401");
+    throw new Error("Error al iniciar sesión");
+  }
+
+  return await response.json();
+};
