95327205ba2d42e0769ea112e78c69a4fcf5c5e8
Goldfinch <facu_vila@hotmail.com>
2025-12-23 12:27:11 -0300

    enviando autorizacion de token al restaurar contraseña

--- app/user/change/password/services/change_service.ts
+++ app/user/change/password/services/change_service.ts
@@ -11,6 +11,7 @@ export const userChangePasswordService = async (data: passwordType, token: strin
             method: "POST",
             headers: {
                 "Content-type": "application/json",
+                "Authorization": `Bearer ${token}`,
             },
             body: JSON.stringify({
                 password: data.password,
