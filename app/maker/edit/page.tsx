ebea8b7ae1c2451f9b40be0c55d7a9ebfc5254e5
Goldfinch <facu_vila@hotmail.com>
2025-12-24 20:20:10 -0300

    se agrego un suspense en el componente de renderizado de la pagina de editar menu

--- app/maker/edit/page.tsx
+++ app/maker/edit/page.tsx
@@ -1,15 +1,26 @@
-'use client'
+"use client";
 
 import { Navbar } from "./components/navbar";
 import { InfoCard } from "./components/Info_Card";
+import { Suspense } from "react";
 
 const editPage = () => {
-  
   return (
     <div className="min-h-screen w-full flex flex-col">
       <Navbar />
       <div className="flex flex-col justify-center items-center py-8 grow">
-        <InfoCard />
+        <Suspense
+          fallback={
+            <div className="w-full max-w-sm mx-auto p-8 text-center">
+              <div className="animate-pulse">
+                <div className="h-48 bg-gray-200 rounded-2xl mb-4"></div>
+                <div className="h-8 bg-gray-200 rounded w-3/4 mx-auto"></div>
+              </div>
+            </div>
+          }
+        >
+          <InfoCard />
+        </Suspense>
       </div>
     </div>
   );
