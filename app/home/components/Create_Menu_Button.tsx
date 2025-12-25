ce7a96599f89d9dbf6ea1dffc345f971cc7013e3
Goldfinch <facu_vila@hotmail.com>
2025-12-25 20:50:57 -0300

    modificacion visual al boton crear menu del home

--- app/home/components/Create_Menu_Button.tsx
+++ app/home/components/Create_Menu_Button.tsx
@@ -6,11 +6,17 @@ import Link from "next/link";
 export const CreateMenuButton = () => {
   return (
     <Link href={"/maker/create"} className="w-full lg:w-auto">
-      <Button className="w-full sm:w-[90%] lg:w-auto lg:min-w-50 lg:max-w-87.5 bg-linear-to-r from-orange-400 to-orange-500 rounded-2xl p-10 sm:p-8 cursor-pointer transition-all duration-300 hover:shadow-2xl active:scale-[0.98] overflow-hidden shadow-lg">
-        <div className=" flex items-center justify-center">
-          <p className="text-white  text-xl sm:text-lg font-semibold">Crear menú</p>
-        </div>
-      </Button>
-    </Link>
+  <Button className="relative w-full sm:w-[90%] lg:w-auto lg:min-w-50 lg:max-w-87.5 bg-gradient-to-r from-orange-400 to-orange-500 rounded-2xl p-10 sm:p-8 cursor-pointer transition-all duration-300 hover:shadow-2xl active:scale-[0.98] overflow-hidden shadow-lg text-left">
+    <div className="absolute right-0 top-0 bottom-0 w-32 opacity-20 pointer-events-none lg:hidden">
+      <div className="w-24 h-24 bg-white rounded-full absolute -right-8 -top-8" />
+      <div className="w-20 h-20 bg-white rounded-full absolute right-4 bottom-0" />
+    </div>
+
+    <div className="relative z-10 flex items-center justify-start w-full">
+      <Plus className="w-8 h-8 text-white mr-3" strokeWidth={3} />
+      <p className="text-white text-xl font-semibold">Crear nuevo menú</p>
+    </div>
+  </Button>
+</Link>
   );
 };
