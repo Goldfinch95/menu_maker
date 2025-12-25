0818a33f01e20011f5054bfaf38e3ad47a2258e0
Goldfinch <facu_vila@hotmail.com>
2025-12-25 01:48:10 -0300

    se agrego un suspense

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,42 +1,37 @@
-"use client"
-import  { useEffect, useState } from 'react';
-import { getPublicMenu } from './services/public_menu_services';
-import { useSearchParams } from 'next/navigation';
-import { Menu } from './types/menu';
+"use client";
+import { useEffect, useState, Suspense } from "react";
+import { getPublicMenu } from "./services/public_menu_services";
+import { useSearchParams } from "next/navigation";
+import { Menu } from "./types/menu";
 
 const menuPage = () => {
   const searchParams = useSearchParams();
-   const id = searchParams.get('id');
+  const id = searchParams.get("id");
   const [menu, setMenu] = useState<Menu | null>(null);
 
-
-    useEffect(() => {
-     if (!id || typeof id !== 'string') return;
+  useEffect(() => {
+    if (!id || typeof id !== "string") return;
 
     const loadMenu = async () => {
-      
       try {
-        
         const data = await getPublicMenu(id);
         console.log(data);
         setMenu(data);
-        
       } catch (error) {
         console.error("❌ Error al cargar el menú:", error);
-        
       }
     };
 
     loadMenu();
   }, [id]);
-    return (
-        <div>
-            <div>Título: {menu?.title}</div>
-      <div>Pos: {menu?.pos}</div>
-            {/* mostrar nombre*/}
-            {/* mostrar pos*/}
-        </div>
-    );
+  return (
+    <div>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <div>Título: {menu?.title}</div>
+        <div>Pos: {menu?.pos}</div>
+      </Suspense>
+    </div>
+  );
 };
 
 export default menuPage;
