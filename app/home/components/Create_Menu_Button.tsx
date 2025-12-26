7b70882a66736b5301c2ba4a2f7544ce1f70814f
Goldfinch <facu_vila@hotmail.com>
2025-12-25 21:01:44 -0300

    Cambios en dev

--- app/home/components/Create_Menu_Button.tsx
+++ app/home/components/Create_Menu_Button.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export const CreateMenuButton = () => {
   return (
     <Link href={"/maker/create"} className="w-full lg:w-auto">
-  <Button className="relative w-full sm:w-[90%] lg:w-auto lg:min-w-50 lg:max-w-87.5 bg-gradient-to-r from-orange-400 to-orange-500 rounded-2xl p-10 sm:p-8 cursor-pointer transition-all duration-300 hover:shadow-2xl active:scale-[0.98] overflow-hidden shadow-lg text-left">
+  <Button className="relative w-full sm:w-[90%] lg:w-auto lg:min-w-50 lg:max-w-87.5 bg-linear-to-r from-orange-400 to-orange-500 rounded-2xl p-10 sm:p-8 cursor-pointer transition-all duration-300 hover:shadow-2xl active:scale-[0.98] overflow-hidden shadow-lg text-left">
     <div className="absolute right-0 top-0 bottom-0 w-32 opacity-20 pointer-events-none lg:hidden">
       <div className="w-24 h-24 bg-white rounded-full absolute -right-8 -top-8" />
       <div className="w-20 h-20 bg-white rounded-full absolute right-4 bottom-0" />
